bff200e9197d6111820fa0ea5c4eab473de72bfa
Gage Bachik <gbachik>
2021-02-15 16:00:54 -0800

    add -A

--- pages/cars/[id].js
+++ pages/cars/[id].js
@@ -18,13 +18,21 @@ export default function Car({ car }) {
   );
 }
 
-export async function getStaticProps({ params }) {
+export async getServerSideProps({params}){
   const req = await fetch(`http://localhost:3000/${params.id}.json`);
   const data = await req.json();
   return {
     props: { car: data },
   };
 }
+
+// export async function getStaticProps({ params }) {
+//   const req = await fetch(`http://localhost:3000/${params.id}.json`);
+//   const data = await req.json();
+//   return {
+//     props: { car: data },
+//   };
+// }
 
 export async function getStaticPaths() {
   const req = await fetch("http://localhost:3000/cars.json");
